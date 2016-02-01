f0143b05d0e6a906fc61fa1f844d2cfd3ff840d2
HeoJooSeong <zoostar1207@gmail.com>
2016-02-01 21:31:44 +0900

    집단지성 3_2

--- LINK_LAB/Hadoop/hadoop_Pdf_Append.sh
+++ LINK_LAB/Hadoop/hadoop_Pdf_Append.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-foldername=/home/link/paper_archive
-
+foldername=/home/link/tmpPdf
+mv /home/link/paper_archive/*.pdf /home/link/tmpPdf/
 
 for LINE in $foldername/* 
 do
@@ -12,11 +12,14 @@ do
 	hadoop fs -copyToLocal /user/root/Pdf_archives/`expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar /home/link/tmpPdf
 	cd /home/link/tmpPdf
 	tar -rf `expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar $filefullname
+	hadoop fs -rm /user/root/Pdf_archives/`expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar
 	hadoop fs -copyFromLocal `expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar /user/root/Pdf_archives
 #	mv /Users/zoostar/Desktop/pdftar/`expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar /Users/zoostar/Desktop/pdf
 #	cd /Users/zoostar/Desktop/pdf
 #	tar -rf `expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar $filefullname
 #	mv  `expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar /Users/zoostar/Desktop/pdftar/
-
+	rm /home/link/tmpPdf/`expr \( $filename / 100 \) \* 100 + 1`_`expr \( \( $filename / 100 \) + 1 \) \* 100`pdf.tar
 done 
+
+mv /home/link/paper_archive/*."pdf" /home/link/paper_archive_Save
 
