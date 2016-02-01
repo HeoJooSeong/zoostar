822d8844de1ca95f796f650b0da08ea99d45c47b
HeoJooSeong <zoostar1207@gmail.com>
2016-02-01 23:39:10 +0900

    집단지성

--- LINK_LAB/집단지성 세미나/ciia/src/java/com/alag/ci/tagcloud/impl/TagCloudImpl.java
+++ LINK_LAB/집단지성 세미나/ciia/src/java/com/alag/ci/tagcloud/impl/TagCloudImpl.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import com.alag.ci.MetaDataVector;
-import com.alag.ci.TagMagnitude;
 import com.alag.ci.tagcloud.FontSizeComputationStrategy;
 import com.alag.ci.tagcloud.TagCloud;
 import com.alag.ci.tagcloud.TagCloudElement;
@@ -27,20 +25,7 @@ public class TagCloudImpl implements TagCloud {
         strategy.computeFontSize(this.elements);
         Collections.sort(this.elements);
     }
-    
-//    public TagCloudImpl(MetaDataVector metaDataVector,
-//                        FontSizeComputationStrategy strategy) {
-//      this(getTagCloudElements(metaDataVector),strategy);
-//    }
-    
-//    private static List<TagCloudElement> getTagCloudElements(MetaDataVector metaDataVector) {
-//        ArrayList<TagCloudElement> list = new ArrayList<TagCloudElement>();
-//        for (TagMagnitude tm : metaDataVector.getTagMetaDataMagnitude()) {
-//            list.add(new TagCloudElementImpl(tm.getTagText(),
-//                    tm.getMagnitude()));
-//        }
-//        return list;
-//    }
+
 
     public List<TagCloudElement> getTagCloudElements() {
        return this.elements;
